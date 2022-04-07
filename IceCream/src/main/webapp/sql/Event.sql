50efd438b3ab508d49a3fb0324578e73cd4b64d6
김은지 <keji9@DESKTOP-0RTT37N>
2022-04-07 17:46:16 +0900

    Event, Notice sql문 수정

--- IceCream/src/main/webapp/sql/Event.sql
+++ IceCream/src/main/webapp/sql/Event.sql
@@ -1,6 +1,6 @@
 -- event(이벤트)			
 create table event (
-	event_seq number primary key,
+    num number primary key,
     title    varchar2(100) not null,
     content  varchar2(2000) not null,
     hit number default 0,
@@ -10,13 +10,12 @@ create table event (
 
 create sequence event_seq nocycle nocache;
 
-select * from event where event_seq=1;
+select * from event where num=1;
 select count(*) from event;
 
 insert into event values (event_seq.nextval, '제목1', '내용1', 0, sysdate, '파일1');
 
-update event set title='', content='', hit=?, logTime='', fileName='' where event_seq=?;
-update event set hit = hit+1 where event_seq=?;
+update event set title='', content='', hit=?, logTime='', fileName='' where num=?;
+update event set hit = hit+1 where num=?;
 
-delete event where event_seq=?;
-
+delete event where num=?;
--- IceCream/src/main/webapp/sql/Notice.sql
+++ IceCream/src/main/webapp/sql/Notice.sql
@@ -1,6 +1,6 @@
 -- notice(공지사항)
 create table notice(
-	notice_seq number primary key,
+    num number primary key,
     title varchar2(100) not null,
     content varchar2(2000) not null,
     hit number default 0,
@@ -10,12 +10,12 @@ create table notice(
 
 create sequence notice_seq nocycle nocache;
 
-select * from notice where notice_seq=1;
+select * from notice where num=1;
 select count(*) from notice;
 
 insert into notice values (notice_seq.nextval, '제목1', '내용1', 0, sysdate, '파일1');
 
-update notice set title='', content='', hit=?, logTime='', fileName='' where notice_seq=?;
-update notice set hit = hit+1 where notice_seq=?;
+update notice set title='', content='', hit=?, logTime='', fileName='' where num=?;
+update notice set hit = hit+1 where num=?;
 
-delete notice where notice_seq=?;
+delete notice where num=?;
