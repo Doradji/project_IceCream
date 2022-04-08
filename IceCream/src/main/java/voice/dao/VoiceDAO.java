b28b7fdca33cef9176e39c46e9ec8992571650ee
Giteak Hong <hgt2768@naver.com>
2022-04-08 15:01:48 +0900

    test commit

--- IceCream/src/main/java/voice/dao/VoiceDAO.java
+++ IceCream/src/main/java/voice/dao/VoiceDAO.java
@@ -3,11 +3,17 @@ package voice.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import voice.dto.VoiceDTO;
 
 @Repository
 public class VoiceDAO {
     @Autowired
     private SqlSessionTemplate sqlSessionTemplate;
 
+    public int insert(VoiceDTO dto){
+        int result = 0;
+
+        return result;
+    }
 
 }
