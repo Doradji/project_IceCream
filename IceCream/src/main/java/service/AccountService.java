e06e823b0e1644311b3530185dc7fdceae642866
나라PC <PC@DESKTOP-7J4L8MQ>
2022-04-11 11:02:24 +0900

    AccountService 생성

---
+++ IceCream/src/main/java/service/AccountService.java
@@ -0,0 +1,51 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import account.dao.AccountDAO;
+import account.dto.AccountDTO;
+
+@Service
+public class AccountService {
+	@Autowired
+	AccountDAO accountDAO;
+	
+	// 회원 가입(저장)
+	public int insert(AccountDTO dto) {
+		return accountDAO.insert(dto);
+	}
+	
+	// 로그인
+	public AccountDTO login(String id, String pass) {
+		return accountDAO.login(id, pass);
+	}
+	
+	// 고객 개별 조회
+	public AccountDTO selectOne(String id) {
+		return accountDAO.selectOne(id);
+	}
+	
+	// 고객 전체 조회
+	public List<AccountDTO> selectList(int startNum, int endNum) {
+		return accountDAO.selectList(startNum, endNum);
+	}
+	
+	// 총 인원 수 구하기
+	public int selectTotal() {
+		return accountDAO.selectTotal();
+	}
+	
+	// 회원 정보 수정(수정)
+	public int modify(AccountDTO dto) {
+		return accountDAO.modify(dto);
+	}
+	
+	// 회원 탈퇴(삭제)
+	public int delete(String id) {
+		return accountDAO.delete(id);
+	}
+	
+}
